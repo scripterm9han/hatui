b0e8ba9c3a6a564cfad4367b269f601720fe758f
Adwait <adwait@example.com>
2026-07-06 23:30:56 +0530

    fix: switch postgresql to sqlite for zero-config deployment on vercel

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,33 @@
 import { PrismaClient } from "@prisma/client";
 
+import fs from "fs";
+import path from "path";
+
 const prismaClientSingleton = () => {
+  if (process.env.VERCEL === "1") {
+    const dbName = "dev.db";
+    const dbPath = path.join("/tmp", dbName);
+    const srcPath = path.join(process.cwd(), "prisma", dbName);
+    
+    process.env.DATABASE_URL = `file:${dbPath}`;
+    
+    if (!fs.existsSync(dbPath)) {
+      try {
+        if (!fs.existsSync("/tmp")) {
+          fs.mkdirSync("/tmp", { recursive: true });
+        }
+        if (fs.existsSync(srcPath)) {
+          fs.copyFileSync(srcPath, dbPath);
+          console.log("Successfully copied SQLite database to /tmp");
+        } else {
+          console.error(`Source database file not found at: ${srcPath}`);
+        }
+      } catch (error) {
+        console.error("Error setting up SQLite database in /tmp:", error);
+      }
+    }
+  }
+
   return new PrismaClient();
 };
 
